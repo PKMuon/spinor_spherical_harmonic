13b9b0dd1fa9d31f2e87bdba732552aac9c71927
lyazj <seeson@pku.edu.cn>
2025-10-29 18:11:05 +0800

    Improve code quality in spinor_spherical_harmonic.cpp

--- spinor_spherical_harmonic.cpp
+++ spinor_spherical_harmonic.cpp
@@ -4,38 +4,46 @@
 #include <array>
 #include <boost/math/special_functions/spherical_harmonic.hpp>
 #include <cmath>
+#include <complex>
 
-static std::complex<double> spherical_harmonic(double l, double m, double theta, double phi)
+using namespace std;
+using namespace complex_literals;
+
+namespace {  // Internal symbols only visible within this translation unit.
+
+complex<double> spherical_harmonic(double l, double m, double theta, double phi)
 {
   return boost::math::spherical_harmonic(round(l), round(m), theta, phi);
 }
 
-static std::array<std::complex<double>, 2> spin_half_spherical_harmonic(
-    double l, double j, double m, double theta, double phi)
+array<complex<double>, 2> spin_half_spherical_harmonic(double l, double j, double m, double theta, double phi)
 {
   if(j > l) {
-    std::complex<double> upper = sqrt((j + m) / (2.0 * j)) * spherical_harmonic(l, m - 0.5, theta, phi);
-    std::complex<double> lower = sqrt((j - m) / (2.0 * j)) * spherical_harmonic(l, m + 0.5, theta, phi);
+    complex<double> upper = sqrt((j + m) / (2.0 * j)) * spherical_harmonic(l, m - 0.5, theta, phi);
+    complex<double> lower = sqrt((j - m) / (2.0 * j)) * spherical_harmonic(l, m + 0.5, theta, phi);
     return { upper, lower };
   } else {
-    std::complex<double> upper = -sqrt((j - m + 1.0) / (2.0 * j + 2.0)) * spherical_harmonic(l, m - 0.5, theta, phi);
-    std::complex<double> lower = sqrt((j + m + 1.0) / (2.0 * j + 2.0)) * spherical_harmonic(l, m + 0.5, theta, phi);
+    complex<double> upper = -sqrt((j - m + 1.0) / (2.0 * j + 2.0)) * spherical_harmonic(l, m - 0.5, theta, phi);
+    complex<double> lower = sqrt((j + m + 1.0) / (2.0 * j + 2.0)) * spherical_harmonic(l, m + 0.5, theta, phi);
     return { upper, lower };
   }
 }
 
-static std::array<std::complex<double>, 2> spin_half_spherical_harmonic(
-    double kappa, double m, double theta, double phi)
+array<complex<double>, 2> spin_half_spherical_harmonic(double kappa, double m, double theta, double phi)
 {
-  double l, j = std::abs(kappa) - 0.5;
+  double l, j = abs(kappa) - 0.5;
   if(kappa > 0) {
-    l = kappa;  // j = l - 0.5
+    l = kappa;  // Corresponds to j = l - 0.5.
   } else {
-    l = -1 - kappa;  // j = l + 0.5
+    l = -1 - kappa;  // Corresponds to j = l + 0.5.
   }
   return spin_half_spherical_harmonic(l, j, m, theta, phi);
 }
 
+}  // namespace
+
+// External interface.
+// Uses the C ABI for compatibility with C, C++, Fortran, and Python.
 extern "C" void spin_half_spherical_harmonic(double y_out[4], double kappa, double m, double theta, double phi)
 {
   auto y = spin_half_spherical_harmonic(kappa, m, theta, phi);
@@ -47,63 +55,64 @@ extern "C" void spin_half_spherical_harmonic(double y_out[4], double kappa, doub
 
 #ifdef NEED_TEST_MAIN
 
+#include <iomanip>
 #include <iostream>
-
-using namespace std::complex_literals;
 
 int main()
 {
-  // Tunable parameters.
+  // Adjustable test parameters.
   double kappa = 2, m = 1.5, theta = M_PI / 4, phi = M_PI / 4;
-  double j = std::abs(kappa) - 0.5;
+  double j = abs(kappa) - 0.5;
 
-  std::complex<double> A[2][2] = { 0 };  // Eq. (21a) in PhysRevC.50.2822
-  std::complex<double> B[2][2] = { 0 };  // Eq. (21b) in PhysRevC.50.2822
+  complex<double> A[2][2] = { 0 };  // Eq. (21a) in Phys. Rev. C 50, 2822.
+  complex<double> B[2][2] = { 0 };  // Eq. (21b) in Phys. Rev. C 50, 2822.
 
   for(double m = -j; m <= j; m += 1.0) {
-    // This is not robust, so please check to confirm that m iterates over [-j, j].
-    std::cout << "j = " << j << " m = " << m << std::endl;
+    // This loop is numerically stable since m can be represented exactly as a double.
+    cout << "j = " << j << " m = " << m << endl;
     auto y = spin_half_spherical_harmonic(kappa, m, theta, phi);
     auto z = spin_half_spherical_harmonic(-kappa, m, theta, phi);
-    std::cout << "y = " << y[0] << ", " << y[1] << std::endl;
-    A[0][0] += y[0] * std::conj(y[0]);
-    A[0][1] += y[0] * std::conj(y[1]);
-    A[1][0] += y[1] * std::conj(y[0]);
-    A[1][1] += y[1] * std::conj(y[1]);
-    B[0][0] += y[0] * std::conj(z[0]);
-    B[0][1] += y[0] * std::conj(z[1]);
-    B[1][0] += y[1] * std::conj(z[0]);
-    B[1][1] += y[1] * std::conj(z[1]);
+    cout << "y = " << y[0] << ", " << y[1] << endl;
+
+    A[0][0] += y[0] * conj(y[0]);
+    A[0][1] += y[0] * conj(y[1]);
+    A[1][0] += y[1] * conj(y[0]);
+    A[1][1] += y[1] * conj(y[1]);
+
+    B[0][0] += y[0] * conj(z[0]);
+    B[0][1] += y[0] * conj(z[1]);
+    B[1][0] += y[1] * conj(z[0]);
+    B[1][1] += y[1] * conj(z[1]);
   }
 
-  std::cout << "A:" << std::endl;
+  cout << "A:" << endl;
   for(size_t i0 = 0; i0 < 2; ++i0) {
-    for(size_t i1 = 0; i1 < 2; ++i1) std::cout << " " << std::setw(30) << A[i0][i1] / ((2 * j + 1) / (8 * M_PI));
-    std::cout << std::endl;
+    for(size_t i1 = 0; i1 < 2; ++i1) cout << " " << setw(30) << A[i0][i1] / ((2 * j + 1) / (8 * M_PI));
+    cout << endl;
   }
 
   A[0][0] = A[1][1] = 1;
   A[0][1] = A[1][0] = 0;
-  std::cout << "A ref:" << std::endl;
+  cout << "A (reference):" << endl;
   for(size_t i0 = 0; i0 < 2; ++i0) {
-    for(size_t i1 = 0; i1 < 2; ++i1) std::cout << " " << std::setw(30) << A[i0][i1];
-    std::cout << std::endl;
+    for(size_t i1 = 0; i1 < 2; ++i1) cout << " " << setw(30) << A[i0][i1];
+    cout << endl;
   }
 
-  std::cout << "B:" << std::endl;
+  cout << "B:" << endl;
   for(size_t i0 = 0; i0 < 2; ++i0) {
-    for(size_t i1 = 0; i1 < 2; ++i1) std::cout << " " << std::setw(30) << B[i0][i1] / (-(2 * j + 1) / (8 * M_PI));
-    std::cout << std::endl;
+    for(size_t i1 = 0; i1 < 2; ++i1) cout << " " << setw(30) << B[i0][i1] / (-(2 * j + 1) / (8 * M_PI));
+    cout << endl;
   }
 
   B[0][0] = cos(theta);
-  B[0][1] = sin(theta) * std::exp(-1.0i * phi);
-  B[1][0] = sin(theta) * std::exp(1.0i * phi);
+  B[0][1] = sin(theta) * exp(-1.0i * phi);
+  B[1][0] = sin(theta) * exp(1.0i * phi);
   B[1][1] = -cos(theta);
-  std::cout << "B ref:" << std::endl;
+  cout << "B (reference):" << endl;
   for(size_t i0 = 0; i0 < 2; ++i0) {
-    for(size_t i1 = 0; i1 < 2; ++i1) std::cout << " " << std::setw(30) << B[i0][i1];
-    std::cout << std::endl;
+    for(size_t i1 = 0; i1 < 2; ++i1) cout << " " << setw(30) << B[i0][i1];
+    cout << endl;
   }
   return 0;
 }
